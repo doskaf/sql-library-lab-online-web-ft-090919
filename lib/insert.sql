24e2333f3dddb4b69691688a9f567b80fb9d110a
Dominique Skaf <doskaf@yahoo.com>
2019-10-16 03:56:39 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,6 +1,6 @@
-INSERT INTO series (id, title, author_id, subgenre_id) VALUES
+INSERT INTO series (id, title, author_id, subgenre_id) VALUES 
 (1, 'Harry Potter', 1, 1);
-INSERT INTO series (id, title, author_id, subgenre_id) VALUES
+INSERT INTO series (id, title, author_id, subgenre_id) VALUES 
 (2, 'The Walking Dead', 2, 2);
 
 INSERT INTO subgenres (id, name) VALUES
