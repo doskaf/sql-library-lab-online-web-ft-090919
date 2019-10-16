d3b665d5394f07393557ec370319b25656d24680
Dominique Skaf <doskaf@yahoo.com>
2019-10-16 04:55:51 +0000

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,46 +1,47 @@
-INSERT INTO series (id, title, author_id, subgenre_id) VALUES
-(1, 'Harry Potter', 1, 1);
-INSERT INTO series (id, title, author_id, subgenre_id) VALUES
+INSERT INTO series (id, title, author_id, subgenre_id) VALUES 
+(1, 'Harry Potter', 1, 1), 
 (2, 'The Walking Dead', 2, 2);
 
 INSERT INTO subgenres (id, name) VALUES
-(1, 'Fantasy');
-INSERT INTO subgenres (id, name) VALUES
+(1, 'Fantasy'), 
 (2, 'Graphic Novel');
 
 INSERT INTO authors (id, name) VALUES
-(1, 'J.K. Rowling');
-INSERT INTO authors (id, name) VALUES
+(1, 'J.K. Rowling'), 
 (2, 'Robert Kirkman');
 
 INSERT INTO books (id, title, year, series_id) VALUES
-(1, "Harry Potter and the Sorcerer's Stone", 1997, 1);
-INSERT INTO books (id, title, year, series_id) VALUES
-(2, "Harry Potter and the Chamber of Secrets", 2002, 1);
-INSERT INTO books (id, title, year, series_id) VALUES
-(3, "Harry Potter and the Prisoner of Azkaban", 2004, 1);
-INSERT INTO books (id, title, year, series_id) VALUES
-(4, "The Walking Dead Volume 1: Days Gone Bye", 2003, 2);
-INSERT INTO books (id, title, year, series_id) VALUES
-(5, "The Walking Dead Volume 2: Miles Behind Us", 2004, 2);
-INSERT INTO books (id, title, year, series_id) VALUES
+(1, "Harry Potter and the Sorcerer's Stone", 1997, 1), 
+(2, "Harry Potter and the Chamber of Secrets", 2002, 1), 
+(3, "Harry Potter and the Prisoner of Azkaban", 2004, 1),
+(4, "The Walking Dead Volume 1: Days Gone Bye", 2003, 2), 
+(5, "The Walking Dead Volume 2: Miles Behind Us", 2004, 2), 
 (6, "The Walking Dead Volume 3: Safety Behind Bars", 2005, 2);
 
 INSERT INTO characters (id, name, motto, species, author_id) VALUES
-(1, "Harry Potter", "The Boy Who Lived", "Human Wizard", 1);
-INSERT INTO characters (id, name, motto, species, author_id) VALUES
-(2, "Hermione Granger", "We could be killed! Or worse, expelled!", "Human Wizard", 1);
-INSERT INTO characters (id, name, motto, species, author_id) VALUES
-(3, "Cedric Diggory", "Always remember this...", "Human Wizard", 1);
-INSERT INTO characters (id, name, motto, species, author_id) VALUES
-(4, "Voldemort", "He Who Must Not Be Named", "Human Wizard", 1);
-INSERT INTO characters (id, name, motto, species, author_id) VALUES
-(5, "Rick Grimes", "Keep hope alive.", "Human", 2);
-INSERT INTO characters (id, name, motto, species, author_id) VALUES
-(6, "Glenn Rhee", "We can make it together.", "Human", 2);
-INSERT INTO characters (id, name, motto, species, author_id) VALUES
-(7, "Hershel Greene", "The Earth is healing itself.", "Human", 2);
-INSERT INTO characters (id, name, motto, species, author_id) VALUES
+(1, "Harry Potter", "The Boy Who Lived", "Human Wizard", 1), 
+(2, "Hermione Granger", "We could be killed! Or worse, expelled!", "Human Wizard", 1), 
+(3, "Cedric Diggory", "Always remember this...", "Human Wizard", 1), 
+(4, "Voldemort", "He Who Must Not Be Named", "Human Wizard", 1), 
+(5, "Rick Grimes", "Keep hope alive.", "Human", 2), 
+(6, "Glenn Rhee", "We can make it together.", "Human", 2), 
+(7, "Hershel Greene", "The Earth is healing itself.", "Human", 2), 
 (8, "Negan", "Wanna give Lucille a kiss?", "Human", 2);
 
-INSERT INTO character_books
+INSERT INTO character_books (id, book_id, character_id) VALUES
+(1, 1, 1), 
+(2, 2, 1), 
+(3, 3, 1),
+(4, 1, 2), 
+(5, 2, 2), 
+(6, 3, 2), 
+(7, 2, 3), 
+(8, 3, 4), 
+(9, 4, 5),
+(10, 5, 5),
+(11, 6, 5), 
+(12, 4, 6),
+(13, 5, 6), 
+(14, 6, 6), 
+(15, 5, 7),
+(16, 6, 8);
